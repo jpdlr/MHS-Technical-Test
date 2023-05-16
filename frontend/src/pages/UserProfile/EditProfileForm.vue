571cdb6f48888bb2eade59f3965abcdb34dd2a5a
Jean-Pierre de la Rey <jp@embryoplus.com>
2023-05-16 17:34:31 +0200

    User Profile Cleanup

--- frontend/src/pages/UserProfile/EditProfileForm.vue
+++ frontend/src/pages/UserProfile/EditProfileForm.vue
@@ -8,22 +8,16 @@
 
       <md-card-content>
         <div class="md-layout">
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Company (disabled)</label>
-              <md-input v-model="disabled" disabled></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>User Name</label>
-              <md-input v-model="username" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
+          <div class="md-layout-item md-small-size-100 md-size-50">
             <md-field>
               <label>Email Address</label>
               <md-input v-model="emailadress" type="email"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Contact No.</label>
+              <md-input v-model="contactNo" type="text"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-50">
@@ -38,40 +32,16 @@
               <md-input v-model="lastname" type="text"></md-input>
             </md-field>
           </div>
-          <div class="md-layout-item md-small-size-100 md-size-100">
-            <md-field>
-              <label>Adress</label>
-              <md-input v-model="address" type="text"></md-input>
-            </md-field>
+            <div class="md-layout-item md-size-100">
+              <md-field maxlength="5">
+                <label>About Me</label>
+                <md-textarea v-model="aboutme"></md-textarea>
+              </md-field>
+            </div>
+            <div class="md-layout-item md-size-100 text-right">
+              <md-button class="md-raised md-success">Update Profile</md-button>
+            </div>
           </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>City</label>
-              <md-input v-model="city" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Country</label>
-              <md-input v-model="country" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>Postal Code</label>
-              <md-input v-model="code" type="number"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-size-100">
-            <md-field maxlength="5">
-              <label>About Me</label>
-              <md-textarea v-model="aboutme"></md-textarea>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-size-100 text-right">
-            <md-button class="md-raised md-success">Update Profile</md-button>
-          </div>
-        </div>
       </md-card-content>
     </md-card>
   </form>
@@ -87,17 +57,13 @@ export default {
   },
   data() {
     return {
-      username: null,
       disabled: null,
       emailadress: null,
       lastname: null,
       firstname: null,
-      address: null,
-      city: null,
-      country: null,
-      code: null,
+      contactNo: null,
       aboutme:
-        "Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.",
+        "I'm THE groomer. The one and only. I'm the best groomer in the world.",
     };
   },
 };
